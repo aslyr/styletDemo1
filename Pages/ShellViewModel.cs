c001c1956b2c168edb5653ecce1a7c66da832540
Administrator <aslyr.co@yahoo.com>
2020-09-02 15:10:00 +0800

    firstcommit

---
+++ Pages/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+using HandyControl.Controls;
+using Stylet;
+
+namespace Company.WpfApplication1.Pages
+{
+    public class ShellViewModel : Screen
+    {
+
+        public void ShowMessage()
+        {   
+           
+        }
+        public string Name { get; set; } = "欢迎管理岗";
+        public int pageIndex { get; set; } = 12;
+        public void PageUpdate()
+        {   
+            MessageBox.Show(pageIndex.ToString());
+        }
+    }
+}
